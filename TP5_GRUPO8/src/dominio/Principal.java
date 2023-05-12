05341d29d4cdd7e244134731e2d7cac31bf68d87
VacaMalenzuela <macarenavalenz@gmail.com>
2023-05-12 17:54:53 -0300

    Creo el main, el JFrame,y un JPanel Agregar peliculas

--- TP5_GRUPO8/src/dominio/Principal.java
+++ TP5_GRUPO8/src/dominio/Principal.java
@@ -1,10 +1,20 @@
 package dominio;
 
+import java.awt.EventQueue;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Programa frame = new Programa();
+					frame.cambiarVisibilidad(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 }
--- TP5_GRUPO8/src/dominio/Programa.java
+++ TP5_GRUPO8/src/dominio/Programa.java
@@ -6,37 +6,43 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class Programa extends JFrame {
 
 	private JPanel contentPane;
+	private static DefaultListModel<Pelicula> listModel;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Programa frame = new Programa();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
 	public Programa() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
+		setTitle("Programa");
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnNewMenuPeliculas = new JMenu("Peliculas");
+		menuBar.add(mnNewMenuPeliculas);
+		
+		JMenuItem mntmNewMenuItemAgregar = new JMenuItem("Agregar");
+		mnNewMenuPeliculas.add(mntmNewMenuItemAgregar);
+		
+		JMenuItem mntmNewMenuItemListar = new JMenuItem("Listar");
+		mnNewMenuPeliculas.add(mntmNewMenuItemListar);
+		
+		
+		// cambiar por funciones de menu
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+	}
+	
+	public void cambiarVisibilidad(boolean estado) {
+		setVisible(true);
 	}
 
 }
